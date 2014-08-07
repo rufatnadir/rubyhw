f29560ecfd6bd5f51c037e9a79c57f195d4e4a08
Rufat Nadir <rufat.nadir@gmail.com>
2014-08-07 01:31:00 -0700

    Homeworks 14 with archive

---
+++ HW_14/script_14_02.rb
@@ -0,0 +1,8 @@
+﻿# script_14_02.rb
+# Display result of the subtraction operation using following variables:  a, b
+
+a = 10
+b = 3
+result = a / b
+
+puts "Result of subtraction #{a} and #{b} is #{result}"
---
+++ HW_14/script_14_04.rb
@@ -0,0 +1,12 @@
+﻿# script_14_04.rb
+# Display result of the division operation (3 results: 1 - integer, 2 - float, 3 - rounded to 2 decimal) using following variables:  a, b
+
+a = 10
+b = 3
+result = a/b
+
+puts "Division of #{a} and #{b}"
+puts "Result in integer: #{result}"
+puts "Result in float: #{a/b.to_f}"
+result = a/b.to_f
+puts "Result in round decimal: #{result.round(2)}"
